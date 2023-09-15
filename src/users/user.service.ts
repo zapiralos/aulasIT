10f9d1d177d55beb04dbcc457fe10bb2a2aa4af3
Juan <JLische-@hotmail.com>
2023-09-15 16:25:05 -0300

    (FEAT) modified user service and added validations

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -6,6 +6,8 @@ import { StatusCodes } from 'http-status-codes';
 import { RESULT_OK, type IResult, NOT_FOUND } from '../utils/interfaces/result.interface';
 import bcrypt from 'bcrypt';
 import { type User } from './user.entity';
+import { unsupportedNameChars } from '../utils/regular-expressions';
+import { Not } from 'typeorm';
 
 const userRepository = new UserRepository();
 
@@ -21,6 +23,53 @@ export class UserService {
         message: `Hubo un error de validación al actualizar el usuario: ${errors}.`,
         entity: null,
         resultKeys: errorKeys
+      };
+    }
+
+    const userFound = await userRepository.findOneBy({ email: userCreateDTO.email });
+
+    if (userFound !== null) {
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'El email ya se encuentra registrado.',
+        entity: null,
+        resultKeys: ['email-already-registered']
+      };
+    }
+
+    if (userCreateDTO.firstName.match(unsupportedNameChars) === null) {
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'El nombre no puede contener caracteres especiales.',
+        entity: null,
+        resultKeys: ['firstname-unsupported-characters']
+      };
+    }
+
+    if (userCreateDTO.lastName.match(unsupportedNameChars) === null) {
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'El apellido no puede contener caracteres especiales.',
+        entity: null,
+        resultKeys: ['lastname-unsupported-characters']
+      };
+    }
+
+    if (userCreateDTO.password !== userCreateDTO.confirmPassword) {
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'Las contraseñas no coinciden, por favor intente de nuevo.',
+        entity: null,
+        resultKeys: ['password-not-match']
+      };
+    }
+
+    if (userCreateDTO.email !== userCreateDTO.confirmEmail) {
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'Hubo un error al confirmar el email, por favor intente de nuevo.',
+        entity: null,
+        resultKeys: ['email-not-match']
       };
     }
 
@@ -85,6 +134,85 @@ export class UserService {
         entity: null,
         resultKeys: [NOT_FOUND]
       };
+    }
+
+    if (userUpdateDTO.email === undefined && userUpdateDTO.confirmEmail !== undefined) {
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'Para actualizar el email, por favor introduzca uno nuevo.',
+        entity: null,
+        resultKeys: ['email-empty']
+      };
+    }
+
+    if (userUpdateDTO.email !== undefined && userUpdateDTO.confirmEmail === undefined) {
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'La confirmación de email es requerida.',
+        entity: null,
+        resultKeys: ['confirm-email-empty']
+      };
+    }
+
+    if (userUpdateDTO.email !== undefined && userUpdateDTO.email !== null) {
+      const isEmailRegistered = await userRepository.findOneBy({ email: userUpdateDTO.email, id: Not(currentUser.id) });
+      if (isEmailRegistered !== null) {
+        return {
+          statusCode: StatusCodes.BAD_REQUEST,
+          message: 'El email ya se encuentra en uso.',
+          entity: null,
+          resultKeys: ['email-already-in-use']
+        };
+      }
+    }
+
+    if (userUpdateDTO.email !== undefined && userUpdateDTO.email !== userUpdateDTO.confirmEmail) {
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'Hubo un error al confirmar el email, por favor intente de nuevo.',
+        entity: null,
+        resultKeys: ['email-not-match']
+      };
+    }
+
+    if (userUpdateDTO.oldPassword === undefined && (userUpdateDTO.password !== undefined || userUpdateDTO.confirmPassword !== undefined)) {
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'Para cambiar la contraseña, por favor ingrese la contraseña actual.',
+        entity: null,
+        resultKeys: ['empty-current-password']
+      };
+    }
+
+    if (userUpdateDTO.oldPassword !== undefined) {
+      const isPasswordCorrect = await bcrypt.compare(userUpdateDTO.oldPassword, currentUser.password);
+      if (!isPasswordCorrect) {
+        return {
+          statusCode: StatusCodes.BAD_REQUEST,
+          message: 'Contraseña actual incorrecta, intente de nuevo.',
+          entity: null,
+          resultKeys: ['incorrect-current-password']
+        };
+      }
+      if (userUpdateDTO.password === undefined || userUpdateDTO.password === null) {
+        return {
+          statusCode: StatusCodes.BAD_REQUEST,
+          message: 'No se proporcionó ninguna contraseña nueva para cambiar la actual, intente de nuevo.',
+          entity: null,
+          resultKeys: ['new-password-not-provided']
+        };
+      }
+      if (userUpdateDTO.confirmPassword !== userUpdateDTO.password) {
+        return {
+          statusCode: StatusCodes.BAD_REQUEST,
+          message: 'Las contraseñas no coinciden, por favor intente de nuevo.',
+          entity: null,
+          resultKeys: ['password-not-match']
+        };
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      userUpdateDTO.password = await bcrypt.hash(userUpdateDTO.password, salt);
     }
 
     const { confirmEmail, confirmPassword, oldPassword, ...userToUpdate } = userUpdateDTO;
---
+++ src/utils/regular-expressions.ts
@@ -0,0 +1,1 @@
+export const unsupportedNameChars = /^[^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;.,:[\]]{2,}$/;
